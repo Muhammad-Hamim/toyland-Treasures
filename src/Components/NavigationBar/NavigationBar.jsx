9515ca3e79665de6095e96a9670752ef6ec02647
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-05-18 17:38:42 +0600

    router setup done

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -1,142 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import { Link, NavLink } from "react-router-dom";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { HiBars3 } from "react-icons/hi2";
 
 const NavigationBar = () => {
+  const [smallDevice, setSmallDevice] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
   const NavItem = (
     <>
-      <li>
-        <NavLink to="/" className="hover:text-gray-200" href="#">
-          Home
-        </NavLink>
+      <li
+        className={`${
+          smallDevice &&
+          "py-2 hover:text-slate-100 hover:bg-violet-400 transition-all px-8 duration-200"
+        }`}>
+        <NavLink to="/">Home</NavLink>
       </li>
-      <li>
-        <NavLink to="/" className="hover:text-gray-200" href="#">
-          Catagory
-        </NavLink>
+      <li
+        className={`${
+          smallDevice &&
+          "py-2 hover:text-slate-100 hover:bg-violet-400 transition-all px-8 duration-200"
+        }`}>
+        <NavLink to="/">Catagory</NavLink>
       </li>
-      <li>
-        <a className="hover:text-gray-200" href="#">
-          Collections
-        </a>
+      <li
+        className={`${
+          smallDevice &&
+          "py-2 hover:text-slate-100 hover:bg-violet-400 transition-all px-8 duration-200"
+        }`}>
+        <NavLink>Collections</NavLink>
       </li>
-      <li>
-        <a className="hover:text-gray-200" href="#">
-          Contact Us
-        </a>
+      <li
+        className={`${
+          smallDevice &&
+          "py-2 hover:text-slate-100 hover:bg-violet-400 transition-all px-8 duration-200"
+        }`}>
+        <NavLink>Contact Us</NavLink>
       </li>
-      <li>
-        <NavLink to="/login" className="hover:text-gray-200" href="#">
-          Login
-        </NavLink>
+      <li
+        className={`${
+          smallDevice &&
+          "py-2 hover:text-slate-100 hover:bg-violet-400 transition-all px-8 duration-200"
+        }`}>
+        <NavLink to="/login">Login</NavLink>
+      </li>
+    </>
+  );
+  const ProfileItem = (
+    <>
+      <li className="cursor-pointer font-bold py-2 hover:text-slate-100 hover:bg-violet-400 transition-all px-8 duration-200">
+        Home
       </li>
     </>
   );
   return (
-    <div className="">
-      <section className="relative mx-auto">
-        {/* navbar */}
-        <nav className="flex justify-between bg-gray-900 text-white w-full">
-          <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-            <Link to="/" className="text-3xl font-bold font-heading">
-              {/* <img class="h-9" src="logo.png" alt="logo"> */}
-              <img className="w-24 h-fit" src={logo} alt="" />
-            </Link>
-            {/* Nav Links */}
-            <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-              {NavItem}
-            </ul>
-            {/* Header Icons */}
-            <div className="hidden xl:flex items-center space-x-5 items-center">
-              <a className="hover:text-gray-200" href="#">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
-              </a>
-              <a className="flex items-center hover:text-gray-200" href="#">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
-                <span className="flex absolute -mt-5 ml-4">
-                  <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75" />
-                  <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
-                </span>
-              </a>
-              {/* Sign In / Register      */}
-              <a className="flex items-center hover:text-gray-200" href="#">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 hover:text-gray-200"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-          {/* Responsive navbar */}
-          <a className="xl:hidden flex mr-6 items-center" href="#">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 hover:text-gray-200"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-            <span className="flex absolute -mt-5 ml-4">
-              <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75" />
-              <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
-            </span>
-          </a>
-          <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 hover:text-gray-200"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </a>
-        </nav>
-      </section>
+    <div className="relative mx-auto">
+      {/* navbar */}
+      <nav className="flex justify-between bg-gray-900 text-white w-full">
+        <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+          {/* logo */}
+          <Link to="/" className="text-3xl font-bold font-heading">
+            <img className="w-24 h-fit" src={logo} alt="" />
+          </Link>
+          {/* Nav Links */}
+          <ul
+            className={`${
+              showProfile
+                ? "absolute top-full right-5 rounded-lg shadow-xl space-y-3 block bg-white text-gray-900"
+                : "hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12"
+            }`}>
+            {NavItem}
+          </ul>
+          {/* Profile Items */}
+          <ul
+            className={`${
+              showProfile
+                ? "absolute top-full right-5 rounded-lg shadow-xl space-y-3 block bg-white text-gray-900"
+                : "hidden"
+            }`}>
+            {ProfileItem}
+          </ul>
+          {/* Header Icons */}
+        </div>
+        {/* Responsive navbar */}
+        <div className="flex px-5 space-x-5">
+          <button
+            className="block md:hidden self-center"
+            onBlur={() => {
+              setSmallDevice(false);
+            }}
+            onClick={() => {
+              setSmallDevice(!smallDevice);
+            }}>
+            <HiBars3 className="text-3xl text-gray-50"></HiBars3>
+          </button>
+          <button
+            className="flex items-center hover:text-gray-200"
+            onClick={() => {
+              setShowProfile(!showProfile);
+            }}>
+            <MdOutlineAccountCircle className="text-2xl"></MdOutlineAccountCircle>
+          </button>
+        </div>
+      </nav>
     </div>
   );
 };
