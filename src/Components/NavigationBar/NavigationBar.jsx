75aac9df9f447b7d1895651c4bcae9111973e175
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-05-18 18:09:12 +0600

    Navlink active problem solve

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -5,48 +5,45 @@ import { MdOutlineAccountCircle } from "react-icons/md";
 import { HiBars3 } from "react-icons/hi2";
 
 const NavigationBar = () => {
-  const [smallDevice, setSmallDevice] = useState(false);
   const NavItem = (
     <>
       <li>
-        <NavLink to="/">Home</NavLink>
+        <NavLink className="text-gray-900" to="/">
+          Home
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/">Catagory</NavLink>
+        <NavLink className="text-gray-900" to="/category">
+          Category
+        </NavLink>
       </li>
-      <li
-        className={`${
-          smallDevice &&
-          "py-2 hover:text-slate-100 hover:bg-violet-400 transition-all px-8 duration-200"
-        }`}>
-        <NavLink>Collections</NavLink>
+      <li>
+        <NavLink className="text-gray-900" to="/collection">
+          Collections
+        </NavLink>
       </li>
-      <li
-        className={`${
-          smallDevice &&
-          "py-2 hover:text-slate-100 hover:bg-violet-400 transition-all px-8 duration-200"
-        }`}>
-        <NavLink>Contact Us</NavLink>
+      <li>
+        <NavLink className="text-gray-900" to="/contact">
+          Contact Us
+        </NavLink>
       </li>
-      <li
-        className={`${
-          smallDevice &&
-          "py-2 hover:text-slate-100 hover:bg-violet-400 transition-all px-8 duration-200"
-        }`}>
-        <NavLink to="/login">Login</NavLink>
+      <li>
+        <NavLink className="text-gray-900" to="/login">
+          Login
+        </NavLink>
       </li>
     </>
   );
   const ProfileItem = (
     <>
       <li>
-        <a>Profile</a>
+        <a className="text-gray-900">Profile</a>
       </li>
       <li>
-        <a>Settings</a>
+        <a className="text-gray-900">Settings</a>
       </li>
       <li>
-        <a>Logout</a>
+        <button className="text-gray-900">Logout</button>
       </li>
     </>
   );
@@ -60,12 +57,15 @@ const NavigationBar = () => {
             <img className="w-24 h-fit" src={logo} alt="" />
           </Link>
           {/* Header Icons */}
-          
+          {/* Navlink */}
+          <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+            {NavItem}
+          </ul>
         </div>
         {/* Responsive navbar */}
         <div className="flex items-center px-5 space-x-5">
           {/* dropdown menu */}
-          <div className="dropdown dropdown-end">
+          <div className="dropdown dropdown-end md:hidden">
             <label tabIndex={1} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
                 <button tabIndex={1} className="btn btn-primary btn-circle">
@@ -75,7 +75,7 @@ const NavigationBar = () => {
             </label>
             <ul
               tabIndex={1}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              className="menu space-y-3 menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
               {NavItem}
             </ul>
           </div>
