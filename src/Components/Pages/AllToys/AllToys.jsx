1d5df40d4fb1c02ec88fd90a7f5c852b53f5f11c
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-05-20 20:22:46 +0600

    added all toy page and show all toy data in a talbe

--- src/Components/Pages/AllToys/AllToys.jsx
+++ src/Components/Pages/AllToys/AllToys.jsx
@@ -1,14 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import useTitle from "../../../hooks/useTitle";
 import { AuthContext } from "../../Providers/AuthProvider";
-import { ThreeCircles } from "react-loader-spinner";
+import { Dna, ThreeCircles } from "react-loader-spinner";
 import AllToyData from "./AllToyData";
 import Swal from "sweetalert2";
 
 const AllToys = () => {
   useTitle("All toys");
   const { user, loading } = useContext(AuthContext);
-
   const [toys, setToys] = useState([]);
   useEffect(() => {
     fetch("https://toyland-treasures-server.vercel.app/toys")
@@ -56,42 +55,46 @@ const AllToys = () => {
   };
   return (
     <div className="w-full max-w-screen min-h-screen bg-[#eff3f8] flex items-start justify-center px-5 py-5">
-      <div className="overflow-x-auto shadow-md sm:rounded-lg w-full">
-        <table className="w-full text-sm text-left text-gray-500 ">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product Photo
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Rating
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody className="items-center">
-            {toys.map((toyData) => {
-              return (
-                <AllToyData
-                  key={toyData._id}
-                  handleDelete={handleDelete}
-                  toyData={toyData}></AllToyData>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      {!user ? (
+        <div className="overflow-x-auto shadow-md sm:rounded-lg w-full">
+          <table className="w-full text-sm text-left text-gray-500 ">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Product Photo
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Product name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Category
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Rating
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody className="items-center">
+              {toys.map((toyData) => {
+                return (
+                  <AllToyData
+                    key={toyData._id}
+                    handleDelete={handleDelete}
+                    toyData={toyData}></AllToyData>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <h2 className="text-gray-900 text-center font-black text-7xl">Data not found. Please log out first to see all toy!</h2>
+      )}
     </div>
   );
 };
--- src/Components/PrivateRoute/PrivateRoute.jsx
+++ src/Components/PrivateRoute/PrivateRoute.jsx
@@ -1,25 +1,21 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
-import { ThreeCircles } from 'react-loader-spinner';
+import { Dna, ThreeCircles } from 'react-loader-spinner';
 
 const PrivateRoute = ({ children }) => {
   const location = useLocation();
   const { user, loading } = useContext(AuthContext);
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen z-50">
-        <ThreeCircles
-          height="100"
-          width="100"
-          color="#570df8"
-          wrapperStyle={{}}
-          wrapperClass=""
+      <div className="flex justify-center items-center h-[90vh] z-50">
+        <Dna
           visible={true}
-          ariaLabel="three-circles-rotating"
-          outerCircleColor=""
-          innerCircleColor=""
-          middleCircleColor=""
+          height="120"
+          width="120"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
         />
       </div>
     );
